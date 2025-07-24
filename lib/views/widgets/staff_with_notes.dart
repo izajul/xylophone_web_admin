7c704edd8fb5819b89a62c71b261904cdbce6172
izajuBYSL <izajul.haque@intelli.global>
2025-07-24 16:49:42 +0600

    staff_with_notes.dart

--- lib/views/widgets/staff_with_notes.dart
+++ lib/views/widgets/staff_with_notes.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../models/note_model.dart';
 import 'note_widget.dart';
@@ -10,6 +11,7 @@ class StaffWithNotesDragNDrop extends StatefulWidget {
   const StaffWithNotesDragNDrop({super.key, required this.notes});
 
   final List<BaseNoteModel> notes;
+  // final List<String> lyrics
 
   @override
   State<StaffWithNotesDragNDrop> createState() =>
@@ -19,115 +21,188 @@ class StaffWithNotesDragNDrop extends StatefulWidget {
 class _StaffWithNotesDragNDropState extends State<StaffWithNotesDragNDrop> {
   final List<BaseNoteModel> notes = [];
 
+  final ScrollController _noteController = ScrollController();
+  final ScrollController _lyricsController = ScrollController();
+
+  bool _isSyncing = false;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _noteController.addListener(() {
+
+
+      if (_isSyncing) return;
+      _isSyncing = true;
+      _lyricsController.jumpTo(_noteController.offset);
+      _isSyncing = false;
+    });
+
+    _lyricsController.addListener(() {
+      if (_isSyncing) return;
+      _isSyncing = true;
+      _noteController.jumpTo(_lyricsController.offset);
+      _isSyncing = false;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 90,
-      child: Stack(
-        children: [
-          /// Fixed horizontal divider for the Staff
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              // alignment: Alignment.bottomCenter
-              // color: Colors.blue,
-              height: 65,
-              child: Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Divider(color: Colors.black54, height: 11),
-                  Divider(color: Colors.black54, height: 11),
-                  Divider(color: Colors.black54, height: 11),
-                  Divider(color: Colors.black54, height: 11),
-                  Divider(color: Colors.black54, height: 11),
-                ],
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          height: 90,
+          child: Stack(
+            children: [
+              /// Fixed horizontal divider for the Staff
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  // alignment: Alignment.bottomCenter
+                  // color: Colors.blue,
+                  height: 65,
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Divider(color: Colors.black54, height: 11),
+                      Divider(color: Colors.black54, height: 11),
+                      Divider(color: Colors.black54, height: 11),
+                      Divider(color: Colors.black54, height: 11),
+                      Divider(color: Colors.black54, height: 11),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ),
 
-          // Fixed vertical divider for the Clef
-          Positioned(
-            top: 0,
-            bottom: 0,
-            child: VerticalDivider(
-              width: 1,
-              color: Colors.black,
-              endIndent: 28,
-              indent: 18,
-            ),
-          ),
+              // Fixed vertical divider for the Clef
+              Positioned(
+                top: 0,
+                bottom: 0,
+                child: VerticalDivider(
+                  width: 1,
+                  color: Colors.black,
+                  endIndent: 28,
+                  indent: 18,
+                ),
+              ),
 
-          // Fixed Treble Clef
-          Padding(
-            padding: const EdgeInsets.only(right: 20),
-            child: Image.asset(
-              'assets/images/png/icon_treble_clef_musical_note.png',
-              // height: 100,
-              width: 50,
-              fit: BoxFit.contain,
-            ),
-          ),
+              // Fixed Treble Clef
+              Padding(
+                padding: const EdgeInsets.only(right: 20),
+                child: Image.asset(
+                  'assets/images/png/icon_treble_clef_musical_note.png',
+                  // height: 100,
+                  width: 50,
+                  fit: BoxFit.contain,
+                ),
+              ),
 
-          Positioned(
-            top: 0,
-            bottom: 0,
-            left: 50,
-            right: 0,
-            child: DragTarget<BaseNoteModel>(
-              onAcceptWithDetails: (details) {
-                print(details.data);
-                setState(() {
-                  notes.add(details.data);
-                });
-              },
-              onWillAcceptWithDetails: (details) {
-                print(details.data);
-                return true;
-              },
-              builder:
-                  (
-                    BuildContext context,
-                    List<BaseNoteModel?> candidateData,
-                    List<dynamic> rejectedData,
-                  ) {
-                    return ReorderableListView(
-                      // padding: EdgeInsets.only(right: 1000),
-                      // physics: NeverScrollableScrollPhysics(),
-                      buildDefaultDragHandles: false,
-                      scrollDirection: Axis.horizontal,
-                      shrinkWrap: true,
-                      onReorder: (int oldIndex, int newIndex) {
-                        setState(() {
-                          if (newIndex > oldIndex) {
-                            newIndex -= 1;
-                          }
-                          final BaseNoteModel item = notes.removeAt(oldIndex);
-                          notes.insert(newIndex, item);
-                        });
-                      },
-                     proxyDecorator: (Widget child, int index, Animation<double> animation){
-                        return GestureDetector(
-                          onLongPress: (){
-                            print("long press");
-                          },
-                          child:child,
-                        );
-                     },
-                      children: [
-                        for (var i = 0; i < notes.length; i++)
-                          ReorderableDragStartListener(
-                            key: Key("$i"),
-                            index: i,
-                            child: NoteWidget(note: notes[i]),
-                          ),
-                      ],
-                    );
+              Positioned(
+                top: 0,
+                bottom: 0,
+                left: 50,
+                right: 0,
+                child: DragTarget<BaseNoteModel>(
+                  onAcceptWithDetails: (details) {
+                    print(details.data);
+                    setState(() {
+                      notes.add(details.data);
+                    });
+                    // _scrollController.jumpTo(_scrollController.offset);
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+
+                      _noteController.animateTo(
+                        _noteController.position.maxScrollExtent,
+                        duration: 100.milliseconds,
+                        curve: Curves.easeIn,
+                      );
+                    });
                   },
+                  onWillAcceptWithDetails: (details) {
+                    print(details.data);
+                    return true;
+                  },
+                  builder:
+                      (
+                        BuildContext context,
+                        List<BaseNoteModel?> candidateData,
+                        List<dynamic> rejectedData,
+                      ) {
+                        return ReorderableListView(
+                          scrollController: _noteController,
+                          // padding: EdgeInsets.only(right: 1000),
+                          // physics: NeverScrollableScrollPhysics(),
+                          physics: const ClampingScrollPhysics(),
+                          buildDefaultDragHandles: false,
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          onReorder: (int oldIndex, int newIndex) {
+                            setState(() {
+                              if (newIndex > oldIndex) {
+                                newIndex -= 1;
+                              }
+                              final BaseNoteModel item = notes.removeAt(
+                                oldIndex,
+                              );
+                              notes.insert(newIndex, item);
+                            });
+                          },
+                          proxyDecorator:
+                              (
+                                Widget child,
+                                int index,
+                                Animation<double> animation,
+                              ) {
+                                return GestureDetector(
+                                  onLongPress: () {
+                                    print("long press");
+                                  },
+                                  child: child,
+                                );
+                              },
+                          children: [
+                            for (var i = 0; i < notes.length; i++)
+                              ReorderableDragStartListener(
+                                key: Key("$i"),
+                                index: i,
+                                child: NoteWidget(note: notes[i]),
+                              ),
+                          ],
+                        );
+                      },
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 20,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 50),
+            child: ListView.builder(
+              physics: const ClampingScrollPhysics(),
+              controller: _lyricsController,
+              scrollDirection: Axis.horizontal,
+              itemCount: 0,
+              // itemCount: notes.length,
+              itemBuilder: (context, idx) {
+                final item = notes[idx];
+                return Padding(
+                  padding: const EdgeInsets.only(right: 10, left: 10),
+                  child: SizedBox(
+                    width: 30,
+                    child: Center(child: Text("L-1")),
+                  ),
+                );
+              },
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
